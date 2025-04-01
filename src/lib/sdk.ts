531e093f00e412671c5a31af526a46d5f255fdf8
Andreas Oee <andy.oeee@gmail.com>
2025-04-01 14:32:22 +0700

    fix: sdk init, db migration

--- src/lib/sdk.ts
+++ src/lib/sdk.ts
@@ -1,7 +1,6 @@
-import { PARENT_URL, YAPP_ENS_NAME } from '@/constants';
+import { PARENT_URL } from '@/constants';
 import YappSDK from '@yodlpay/yapp-sdk';
 
 export const sdk = new YappSDK({
-  ensName: YAPP_ENS_NAME,
   origin: PARENT_URL,
 });
