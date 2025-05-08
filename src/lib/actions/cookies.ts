bbc9f3fde44573edf5b828e9b4ace4a897ff43db
Andreas Oee <andy.oeee@gmail.com>
2025-05-08 13:48:24 +0700

    test: set random cookie as placeholder

--- src/lib/actions/cookies.ts
+++ src/lib/actions/cookies.ts
@@ -7,15 +7,16 @@ export async function setPlaceholderCookies() {
   const cookieStore = await cookies();
 
   const cookieNames = [
-    process.env.NODE_ENV === 'production'
-      ? '__Secure-next-auth.session-token'
-      : 'next-auth.session-token',
-    process.env.NODE_ENV === 'production'
-      ? '__Secure-next-auth.csrf-token'
-      : 'next-auth.csrf-token',
-    process.env.NODE_ENV === 'production'
-      ? '__Secure-next-auth.callback-url'
-      : 'next-auth.callback-url',
+    // process.env.NODE_ENV === 'production'
+    //   ? '__Secure-next-auth.session-token'
+    //   : 'next-auth.session-token',
+    // process.env.NODE_ENV === 'production'
+    //   ? '__Secure-next-auth.csrf-token'
+    //   : 'next-auth.csrf-token',
+    // process.env.NODE_ENV === 'production'
+    //   ? '__Secure-next-auth.callback-url'
+    //   : 'next-auth.callback-url',
+    'some-cookie',
   ];
 
   cookieNames.forEach((name) => {
